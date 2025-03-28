eea3b7bdad96d7b589c157d19e689c274b7c67c1
Bidesh <sahabidesh523@gmail.com>
2025-03-28 08:11:15 +0530

    input tags and usestate created.

--- formappreact/src/App.js
+++ formappreact/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  // const [firstname, setFirstName] = useState("");
+  // const [lastname, setLastName] = useState("");
+
+  // function changeFirstNameHandler(event){
+  //   // console.log(event.target.value);
+  //   setFirstName(event.target.value);
+  // }
+  // function changeLastNameHandler(event){
+  //   // console.log(event.target.value);
+  //   setLastName(event.target.value);
+  // }
+
+  const [formData, setFormData] = useState({firstName:"",lastName:"", email:"", comments:""});
+
+  function changeHandler(event){
+    setFormData(prevFormData => {
+      return{
+        ...prevFormData,
+        [event.target.name] : [event.target.value]
+      }
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input
+          type='text'
+          placeholder='first name'
+          onChange={changeHandler}
+          name="firstName"
+          value={formData.firstName}
+          />
+        <br></br>
+        <input
+          type='text'
+          placeholder='last name'
+          onChange={changeHandler}
+          name="lastName"
+          value={formData.lastName}
+          />
+          <br></br>
+        <input
+          type='email'
+          placeholder='Enter your email here'
+          onChange={changeHandler}
+          name="email"
+          value={formData.email}
+          />
+          <br></br>
+          <textarea
+          placeholder='enter your commnents here'
+          onChange={changeHandler}
+          name="comments"
+          value={formData.comments}
+          />
+      </form>
     </div>
   );
 }
